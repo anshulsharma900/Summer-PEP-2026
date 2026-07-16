a978766bd26d1f9cbe820a67a18c45a1ec98f26d
Anshul Sharma <anshul031003@gmail.com>
2026-07-16 10:04:44 +0530

    Auto commit

---
+++ basics.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    // data types and variables
+    // int age;
+    // cin>>age;
+    // char name = 'A';
+    // bool ans = false;
+    // int number;
+    // cin>>number;
+
+    // int = 4 bytes
+    // long long = 8 bytes
+    // float = 4 bytes
+    // double = 8 bytes
+    // char = 1 byte
+    // bool = 1 byte
+
+    // Conditional statements
+    // if(age>=18 && number>10){
+    //     cout<<"test conducted"<<endl;
+    // }
+    // else{
+    //     cout<<"test not conducted"<<endl;
+    // }
+
+    // switch case
+    // char grade='A';
+
+    // switch(grade){
+    //     case 'A':
+    //         cout<<"Excellent"<<endl;
+    //         break;
+    //     case 'B':
+    //         cout<<"Good"<<endl;
+    //         break;
+    //     case 'C':
+    //         cout<<"Average"<<endl;
+    //         break;
+    //     default:
+    //         cout<<"Invalid grade"<<endl;
+    // }
+
+    int x=10, y=20;
+
+    (x>y)? cout<<x<<endl : cout<<y<<endl;
+
+    // loops 
+    // for loop
+     int i=0;
+    // for( i=0; i<10;i++){
+    //     cout<<i<<endl;
+    // }
+
+    // while loop
+    while(i<5){
+        cout<<i<<endl;
+        i++;
+    }
+
+    // // do while loop
+    // do{
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // while(i<10);
+
+    // cout<<age<<endl;
+    // cout<<name<<endl;
+    // cout<<ans<<endl;
+    return 0;
+}
