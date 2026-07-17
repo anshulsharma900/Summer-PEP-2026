071df47f7b3f3ad0fb08b3e91c7b26c92da6c506
Anshul Sharma <anshul031003@gmail.com>
2026-07-17 09:45:12 +0530

    Auto commit

--- TREES/heightoftree.cpp
+++ TREES/heightoftree.cpp
@@ -1,5 +1,30 @@
 https://www.geeksforgeeks.org/problems/height-of-binary-tree/1 
 
-#include<iostream>
-using namespace std;
+/*
+Definition for Node
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int height(Node* root) {
+        // code here
+        if(root==NULL) return -1;
+        
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        
+        return 1+ max(leftHeight, rightHeight);
+    }
+};
 
