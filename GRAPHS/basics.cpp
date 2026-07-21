606aed8f211cc3837287a3f1dacba3fc46c934a9
Anshul Sharma <anshul031003@gmail.com>
2026-07-21 10:30:22 +0530

    Auto commit

---
+++ GRAPHS/basics.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+int main(){
+    int n;    
+    int m;
+
+    cout<<"Enter the number of vertices: "<<endl;
+    cin>>n;
+    cout<<"Enter number of edges: "<<endl;
+    cin>>m;
+
+    // to store key, vale make map of list 
+    unordered_map<int, list<int>> adjList;
+    cout<<"Enter edges (u,v): "<<endl;
+
+    for(int i=0; i<n; i++){
+        int u, v;
+        cin>>u>>v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+    return 0;
+}
